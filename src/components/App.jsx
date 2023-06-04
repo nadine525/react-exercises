86d12c46d008b886dbee6cc97684e316f98e4dfc
nadine525 <02_seeders_wide@icloud.com>
2023-06-05 00:02:25 +0200

    app class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,53 @@
-import React from "react";
-import Counter from './Counter';
-import Dropdown from './Dropdown';
-import ColorPicker from "./ColorPicker";
+import React, {Component} from "react";
+// import Counter from './Counter';
+// import Dropdown from './Dropdown';
+// import ColorPicker from "./ColorPicker";
+import TodoList from './TodoList';
 
 
-const colorPickerOptions = [
-  { label: 'red', color: '#F44336' },
-  { label: 'green', color: '#4CAF50' },
-  { label: 'blue', color: '#2196F3' },
-  { label: 'grey', color: '#607D8B' },
-  { label: 'pink', color: '#E91E63' },
-  { label: 'indigo', color: '#3F51B5' },
-];
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#F44336' },
+//   { label: 'green', color: '#4CAF50' },
+//   { label: 'blue', color: '#2196F3' },
+//   { label: 'grey', color: '#607D8B' },
+//   { label: 'pink', color: '#E91E63' },
+//   { label: 'indigo', color: '#3F51B5' },
+// ];
 
 
-export const App = () => {
-  return (
-    <>
-    <Counter initialValue={10} />
-    <Dropdown />
-    <ColorPicker options={ colorPickerOptions} />
+class App extends Component {
+  state = {
+    todos: [],
+  };
+
+
+  render() {
+    const { todos } = this.state;
+    return (
+    
+        <>
+      {/* <Counter initialValue={10} /> */}
+      {/* <Dropdown /> */}
+      {/* <ColorPicker options={colorPickerOptions} /> */}
+        <TodoList todos={todos} />
     </>
+    )
+  }
+  
+}
 
-  );
-};
+// export const App = () => {
+//   return (
+//     <>
+//     {/* <Counter initialValue={10} /> */}
+//     {/* <Dropdown /> */}
+//       {/* <ColorPicker options={colorPickerOptions} /> */}
+//       <TodoList />
+//     </>
+
+//   );
+// };
+
+
+export default App;
